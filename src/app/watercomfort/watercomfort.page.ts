fe191680d9aab599b90b876637c8b08fac08c3cc
kaji <kaji.vertex@gmail.com>
2020-10-17 19:29:19 +0800

    swimming101

--- src/app/watercomfort/watercomfort.page.ts
+++ src/app/watercomfort/watercomfort.page.ts
@@ -14,7 +14,7 @@ export class WatercomfortPage implements OnInit {
         "id": 1,
         "title": "Freestyle!",
         "desc1": "Freestyle Stroke also called front call by many is the most used stroke by the swimmers and triathletes. To swim freestyle, your arms should make alternate movements, one arm moves backward from the head, the other arm comes out of the water from the hips towards the head.",
-        "desc2": "During the ar0m movement, the legs do the flutter kick making upward and downward movements with a pointed toe. This is a result-oriented kicking exercise to strengthen the ligament. Make sure the head is in line with the trunk looking straight down, extended arm with your palms turned downward.",
+        "desc2": "During the arm movement, the legs do the flutter kick making upward and downward movements with a pointed toe. This is a result-oriented kicking exercise to strengthen the ligament. Make sure the head is in line with the trunk looking straight down, extended arm with your palms turned downward.",
         "credit": ["Jim De Ramos (Photo)"," sportzcrazzy (content)"],
         "imageToURL": "assets/images/strokes/freestyle.jpeg",
       },
